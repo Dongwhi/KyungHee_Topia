8c016f2597192952845bfb897e38717ef22abd87
DONGWHI <100705088+Dongwhi@users.noreply.github.com>
2024-05-31 12:41:55 +0900

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,9 @@ void main() async {
 
   var boxchatting = await Hive.openBox('chatting_info'); // 채팅 박스
   // boxchatting.clear();
+  // boxchatting.put('1층 채팅방', [{'Chris White': 'Hello!'}]);
   // boxchatting.put('2층 채팅방', [{'Jane Smith': 'Hello!'}, {'Karen Harris':'let\'s go'}]);
+  // boxchatting.put('3층 채팅방', [{'Emily Davis': 'Hello!'}, {'David Johnson':'Hi!'}]);
   // boxchatting.put('317호', [{'Emily Davis': '빨래 언제할까?'}, {'Robert Wilson':'7시에 하자'}]);
 
   runApp(const MyApp());
@@ -44,6 +46,31 @@ class MyApp extends StatelessWidget { // 가장 바탕에 깔리는 위젯
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) { // build 함수 실행(출력은 widget)
+    // final box = Hive.box<User>('user_info1');
+    // final boxwaiting = Hive.box('waiting_info');
+    // final boxchatting = Hive.box('chatting_info');
+
+    // box.put('John Doe', User('John Doe', 'Johnny', 'john_doe_001', 'pass1234', 2, false, [0, 0, 0, 0, 0], 0, 
+    // ['Jane Smith', 'Michael Brown', 'Emily Davis', 'Robert Wilson', 'Lisa Taylor', 'David Johnson', 'Amy Clark', 'Chris White'], ['2층 채팅방']));
+    // box.put('Jane Smith', User('Jane Smith', 'Janie', 'jane_smith_002', 'pass5678', 2, false, [0, 0, 0, 0, 0], 0, [], ['2층 채팅방']));
+    // box.put('Michael Brown', User('Michael Brown', 'Mike', 'michael_brown_003', 'password9876', 1, false, [0, 0, 0, 0, 0], 0, [], ['1층 채팅방']));
+    // box.put('Emily Davis', User('Emily Davis', 'Em', 'emily_davis_004', 'pass4321', 3, false, [0, 0, 0, 0, 0], 0, [], ['3층 채팅방']));
+    // box.put('Robert Wilson', User('Robert Wilson', 'Rob', 'robert_wilson_005', 'pass8765', 3, false, [0, 0, 0, 0, 0], 0, [], ['3층 채팅방']));
+    // box.put('Lisa Taylor', User('Lisa Taylor', 'Liza', 'lisa_taylor_006', 'password5432', 3, false, [0, 0, 0, 0, 0], 0, [], ['3층 채팅방']));
+    // box.put('David Johnson', User('David Johnson', 'Dave', 'david_johnson_007', 'pass2345', 3, false, [0, 0, 0, 0, 0], 0, [], ['3층 채팅방']));
+    // box.put('Amy Clark', User('Amy Clark', 'Ames', 'amy_clark_008', 'pass6789', 1, false, [0, 0, 0, 0, 0], 0, [], ['1층 채팅방']));
+    // box.put('Chris White', User('Chris White', 'Chris', 'chris_white_009', 'pass0987', 1, false, [0, 0, 0, 0, 0], 0, [], ['1층 채팅방']));
+    // box.put('Karen Harris', User('Karen Harris', 'Kari', 'karen_harris_010', 'pass7654', 2, false, [0, 0, 0, 0, 0], 0, [], ['2층 채팅방']));
+    
+    // boxwaiting.clear(); // 대기시간 정보 초기화
+    // boxwaiting.put(1, [33, 0, 0, 0, 12]);
+    // boxwaiting.put(2, [0, 32, 120, 25, 84]);
+    // boxwaiting.put(3, [47, 25, 78, 47, 0]);
+
+    // boxchatting.clear();
+    // boxchatting.put('2층 채팅방', [{'Jane Smith': 'Hello!'}, {'Karen Harris':'let\'s go'}]);
+    // boxchatting.put('317호', [{'Emily Davis': '빨래 언제할까?'}, {'Robert Wilson':'7시에 하자'}]);
+
     return const MaterialApp(
       title: 'Kyung-Hee Topia',
       // theme: ThemeData(
